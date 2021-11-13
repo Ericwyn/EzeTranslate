03b968a0b49ed242d29ab013079a0c57e3f72980
Ericwyn <ericwyn.chen@gmail.com>
2021-11-13 20:55:44 +0800

    添加 linux 构建脚本

--- build.sh
+++ build.sh
@@ -1,1 +1,20 @@
+echo "start build"
 fyne package -icon ./res-static/icon/icon.png
+
+echo "copy files to build-target"
+mkdir ./build-target
+
+echo "move build target..."
+mv ./EzeTranslate build-target/
+echo ""
+
+echo "copy resource"
+cp -rf ./res-static/ build-target/
+echo ""
+
+echo "copy config"
+cp ./config.yaml ./build-target/
+cp -rf .conf ./build-target/
+echo ""
+
+echo "build success, you can open binary file in build-target"
